e63885a609500f1af9fbe605315fbc3f905eab64
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-19 04:03:19 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -24,4 +24,28 @@ ALTER COLUMN description SET NOT NULL;
 INSERT INTO parts (id, code, manufacturer_id)
 VALUES (54, 'V1-009', 9); // throws ERROR:  null value in column “description” violates not-null constraint
 
+// Ensuring that price_usd and quantity are both NOT NULL in the reorder_options table;
+
+ALTER TABLE reorder_options
+ALTER COLUMN price_usd SET NOT NULL;
+
+ALTER TABLE reorder_options
+ALTER COLUMN quantity SET NOT NULL;
+
+// Ensuring that price_usd and quantity are both positive:
+
+ALTER TABLE reorder_options
+ADD CHECK (price_usd > 0 AND quantity > 0);
+
+// Adding a constraint to reorder_options that limits price per unit between 0.02 USD and 25.00 USD assuming that price per unit for a given ordering option is the price divided by the quantity:
+
+ALTER TABLE reorder_options
+ADD CHECK (price_usd/quantity > 0.02 AND price_usd/quantity < 25);
+
+// Adding a constraint to ensure that we don’t have pricing information on parts that aren’t already tracked in our DB schema:
+
+
+
+
+
 
